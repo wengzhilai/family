af0ca92808956bd53b64e5e929f1d007c358506c
WengZhiLai <3188894@qq.com>
2017-12-22 18:06:05 +0800

    修改

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
 #!venv/bin/python
 from app import app
-print(__name__)
-app.run(host='0.0.0.0',port=5000,debug =True)
+app.run(host='0.0.0.0', port=5000, debug=False)
--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 import os, subprocess, sys
-#subprocess.call(['python3:', 'virtualenv.py', 'flask'])
-#if sys.platform == 'win32':
-#    bin = 'Scripts'
-#else:
 subprocess.call(['pip', 'install', 'flask'])
 subprocess.call(['pip', 'install', 'flask-login'])
 subprocess.call(['pip', 'install', 'flask-openid'])
@@ -16,4 +12,3 @@ subprocess.call(['pip', 'install', 'flask-wtf'])
 subprocess.call(['pip', 'install', 'flask-babel'])
 subprocess.call(['pip', 'install', 'flask-cors'])
 subprocess.call(['pip', 'install', 'Flask-HTTPAuth'])
-#subprocess.call(['pip', 'install', 'flup'])
