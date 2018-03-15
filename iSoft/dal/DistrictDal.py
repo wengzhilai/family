f937f40fa769da209aa128a4ca121c17dfc78bc6
WengZhiLai <3188894@qq.com>
2018-03-15 18:05:38 +0800

    修改可以添加组织结构

--- iSoft/dal/DistrictDal.py
+++ iSoft/dal/DistrictDal.py
@@ -5,25 +5,35 @@ from iSoft.core.Fun import Fun
 
 
 class DistrictDal(FaDistrict):
-
     def __init__(self):
         pass
 
-    def district_findall(self,pageIndex, pageSize, criterion, where):
-        relist,is_succ=Fun.model_findall(FaDistrict, pageIndex, pageSize, criterion, where)
-        return relist,is_succ
-
+    def district_findall(self, pageIndex, pageSize, criterion, where):
+        relist, is_succ = Fun.model_findall(FaDistrict, pageIndex, pageSize,
+                                            criterion, where)
+        return relist, is_succ
 
     def district_Save(self, in_dict, saveKeys):
-        relist,is_succ=Fun.model_save(FaDistrict, self, in_dict, saveKeys)
-        return relist,is_succ
+        if "PARENT_ID" not in in_dict or in_dict["PARENT_ID"] is None:
+            in_dict["LEVEL_ID"] = 0
+            in_dict["ID_PATH"] = "."
+            in_dict["REGION"] = 0
+        else:
+            single, is_succ = self.district_single(in_dict["PARENT_ID"])
+            if single is None:
+                return None, AppReturnDTO(False, "上级节点有误")
+            in_dict["LEVEL_ID"] = single.LEVEL_ID + 1
+            in_dict["ID_PATH"] = single.ID_PATH + in_dict["PARENT_ID"] + "."
+            in_dict["REGION"] = single.REGION
+            pass
+
+        relist, is_succ = Fun.model_save(FaDistrict, self, in_dict, saveKeys)
+        return relist, is_succ
 
     def district_delete(self, key):
-        is_succ=Fun.model_delete(FaDistrict, key)
+        is_succ = Fun.model_delete(FaDistrict, key)
         return is_succ
 
     def district_single(self, key):
-        relist,is_succ=Fun.model_single(FaDistrict, key)
-        return relist,is_succ
-
-            
+        relist, is_succ = Fun.model_single(FaDistrict, key)
+        return relist, is_succ
